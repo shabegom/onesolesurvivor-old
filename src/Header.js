8bd2842bb371f6b071166678c4e4d3741f2c7e83
sbgood <sam@sbgood.ca>
2021-09-05 13:30:03 -0400

    fix header on mobile

--- src/Header.js
+++ src/Header.js
@@ -2,20 +2,19 @@ import React, { Component } from 'react'
 import logo from './survivor-logo.WEBP'
 
 const imageStyle = {
-    width: "15%"
+    width: "20%",
+    minWidth: "150px"
 }
 
 class Header extends Component {
     render() {
         return (
             <div className="header">
-                <img alt='Survivor Logo' src={logo} style={imageStyle} />
-                <div>
-                <div className="site-header">
-                    <h1>One Sole Survivor</h1>
-                    </div>
-                <h2 className="subhead">Out pick, Out watch, Out score </h2>
+                    <div className="site-header">
+                    <h1 className="site-name">One Sole Survivor</h1>
                 </div>
+                                <img alt='Survivor Logo' src={logo} style={imageStyle} className="survivor-logo" />
+                <h2 className="subhead">Out pick, Out watch, Out score </h2>
             </div>
         )
     }
