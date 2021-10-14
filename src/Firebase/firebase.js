274073367eb6337d72b1f1d200f94b364f1dbe1c
Sam <sam@shbgm.ca>
2021-10-13 22:22:56 -0400

    fb update

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -37,8 +37,8 @@ function Firebase(auth, db) {
 
   const setCastaways = updatedCastaways =>
     getCastaways().update(updatedCastaways);
-  const setTribal = points => getTribal(points.num).update(points);
-  const setMerged = isMerged => getState().update({ "/merged/": isMerged });
+  const setTribal = points => getTribal(points.num).set(points);
+  const setMerged = isMerged => getState().set({ "/merged/": isMerged });
   const setIdols = updatedIdols =>
     getState().update({ "/hasIdol/": updatedIdols });
   const setTeams = teams => getTeams().update(teams);
